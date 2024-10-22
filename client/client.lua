dc9c66a314ea7571d3e9d92395f9d52e941af442
Apollyon <37031311+JusCampin@users.noreply.github.com>
2024-10-21 21:47:14 -0700

    Update client.lua
    - update to current core exports
    - add callback to check and set robberies enabled/disabled
    - change distance checks and coords to use vector3 coords
    - move loot countdown to separate event using seconds
    - update out of range and dead checks
    - update enemy ped functionality
    - housekeeping

--- client/client.lua
+++ client/client.lua
@@ -1,100 +1,122 @@
---Variables
-Inmission = false
-local robberyenable = false
+local Core = exports.vorp_core:GetCore()
+local BccUtils = exports['bcc-utils'].initiate()
+local MiniGame = exports['bcc-minigames'].initiate()
 
+local RobberyEnabled, Inmission = false, false
+local DoReset, Countdown = false, false
+local Timer = 0
 
------ Registering Command To enable and disable robberies ----
+-- Command to Enable/Disable Robberies
 RegisterCommand(Config.RobberyCommand, function()
-    TriggerServerEvent('bcc-robbery:JobCheck')
-    TriggerServerEvent('bcc-robbery:CheckPolice')
-end)
-
---Event the serv will trigger if the player is not a restricted job
-RegisterNetEvent('bcc-robbery:RobberyEnabler', function()
-    if not robberyenable then
-        robberyenable = true
-        VORPcore.NotifyRightTip(_U('RobberyEnable'), 4000)
+    if not RobberyEnabled then
+        local result = Core.Callback.TriggerAwait('bcc-robbery:RobberyCheck')
+        if result then
+            RobberyEnabled = true
+            Core.NotifyRightTip(_U('RobberyEnable'), 4000)
+        else
+            RobberyEnabled = false
+        end
     else
-        robberyenable = false
-        VORPcore.NotifyRightTip(_U('RobberyDisable'), 4000)
+        RobberyEnabled = false
+        Core.NotifyRightTip(_U('RobberyDisable'), 4000)
     end
-end)
+end, false)
 
------ Thread to trigger the events -----
 CreateThread(function()
-    for k, v in pairs(Config.Robberies) do
-        TriggerEvent('bcc-robbery:MainHandler', v)
-    end
-end)
-
------- Main Event Handler Triggers the robberies ------
-AddEventHandler('bcc-robbery:MainHandler', function(v)
+    local playerCoords
     while true do
-        Wait(200)
-        if robberyenable then
-            local plc = GetEntityCoords(PlayerPedId())
-            if GetDistanceBetweenCoords(v.StartingCoords.x, v.StartingCoords.y, v.StartingCoords.z, plc.x, plc.y, plc.z) < 5 then
-                while true do
-                    Wait(5)
-                    if GetDistanceBetweenCoords(v.StartingCoords.x, v.StartingCoords.y, v.StartingCoords.z, plc.x, plc.y, plc.z) > 5 then break end
-                    if not Inmission then
-                        if IsPedShooting(PlayerPedId()) then
-                            TriggerServerEvent('bcc-robbery:ServerCooldownCheck', v.Id, v)
-                            Wait(5000) break
-                        end
+        local sleep = 1000
+        local playerPed = PlayerPedId()
+
+        if not RobberyEnabled or Inmission or IsEntityDead(playerPed) then goto END end
+
+        playerCoords = GetEntityCoords(playerPed)
+
+        for _, locationCfg in pairs(Locations) do
+            local distance = #(playerCoords - locationCfg.StartingCoords)
+            if distance <= locationCfg.Distance then
+                sleep = 0
+                if IsPedShooting(playerPed) then
+                    local result = Core.Callback.TriggerAwait('bcc-robbery:CheckCooldown', locationCfg)
+                    if result then
+                        Inmission = true
+                        TriggerEvent('bcc-robbery:RobberyHandler', locationCfg)
+                    else
+                        Core.NotifyRightTip(_U('OnCooldown'), 4000)
+                        Wait(5000)
                     end
                 end
             end
         end
+        ::END::
+        Wait(sleep)
     end
 end)
 
----------- Robbery Setup -----------
-RegisterNetEvent('bcc-robbery:RobberyHandler', function(v)
-    Inmission = true
-    TriggerEvent('bcc-robbery:DeadCheck', v.StartingCoords)
-    VORPcore.NotifyRightTip(_U('RobberyStart'), 4000)
+local function ResetRobbery()
+    Inmission = false
+    Countdown = false
+    Core.NotifyRightTip(_U('RobberyFail'), 4000)
+    Wait(5000)
+    DoReset = false
+end
 
-    if v.EnemyNpcs then
-        TriggerEvent('bcc-robbery:EnemyPeds', v.EnemyNpcCoords)
+AddEventHandler('bcc-robbery:RobberyHandler', function(locationCfg)
+    Core.NotifyRightTip(_U('RobberyStart'), 4000)
+
+    if locationCfg.EnemyNpcs then
+        TriggerEvent('bcc-robbery:EnemyPeds', locationCfg)
     end
 
-    local waittimer = v.WaitBeforeLoot
+    TriggerEvent('bcc-robbery:Countdown', locationCfg)
+    local startingCoords = locationCfg.StartingCoords
     while true do
-        Wait(10)
-        local pl = GetEntityCoords(PlayerPedId())
+        Wait(0)
+        local playerPed = PlayerPedId()
+        local playerCoords = GetEntityCoords(playerPed)
+        local distance = #(playerCoords - startingCoords)
 
-        if PlayerDead then break end
+        if (distance > 30) or (IsEntityDead(playerPed)) then
+            DoReset = true
+            break
+        end
 
-        waittimer = waittimer - 20
-        local roundedtimer = waittimer / 60000
-        local rounded2 = (math.floor(roundedtimer * 100) / 100)
-
-        BccUtils.Misc.DrawText3D(pl.x, pl.y, pl.z, _U('HoldOutBeforeLooting') .. ' ' .. tostring(rounded2) .. ' ' .. _U('HoldOutBeforeLooting2'))
-        if waittimer <= 0 then
-            VORPcore.NotifyRightTip(_U('LootMarked'), 4000) break
+        BccUtils.Misc.DrawText3D(playerCoords.x, playerCoords.y, playerCoords.z + 0.5, _U('HoldOutBeforeLooting') .. ' ' .. tostring(Timer) .. ' ' .. _U('HoldOutBeforeLooting2'))
+        if Timer <= 0 then
+            Core.NotifyRightTip(_U('LootMarked'), 4000)
+            break
         end
     end
 
-    if PlayerDead then
-        Inmission = false
-        VORPcore.NotifyRightTip(_U('RobberyFail'), 4000)
-        Wait(5000)
-        PlayerDead = false return
+    if DoReset then
+        ResetRobbery()
+        return
     end
 
-
-    for k, e in pairs(v.LootLocations) do
-        TriggerEvent('bcc-robbery:LootHandler', e)
+    for _, lootCfg in pairs(locationCfg.LootLocations) do
+        TriggerEvent('bcc-robbery:LootHandler', lootCfg)
     end
 end)
 
----------- Loot Locations Setup ---------
-AddEventHandler('bcc-robbery:LootHandler', function(e)
+AddEventHandler('bcc-robbery:Countdown', function(locationCfg)
+    Countdown = true
+    Timer = locationCfg.WaitBeforeLoot
+    while Countdown do
+        Wait(1000)
+        Timer = Timer - 1
+        if Timer <= 0 then
+            Countdown = false
+        end
+    end
+end)
+
+AddEventHandler('bcc-robbery:LootHandler', function(lootCfg)
     math.randomseed(GetGameTimer()) --Create a new seed for math.random
 
-    local PromptGroup = VORPutils.Prompts:SetupPromptGroup() --registers a prompt group using vorp_utils
-    local firstprompt = PromptGroup:RegisterPrompt(_U('Rob'), 0x760A9C6F, 1, 1, true, 'hold', {timedeventhash = "MEDIUM_TIMED_EVENT"})
+    local lootGroup = BccUtils.Prompts:SetupPromptGroup()
+    local lootPrompt = lootGroup:RegisterPrompt(_U('Rob'), Config.Keys.Loot, 1, 1, true, 'hold', {timedeventhash = "MEDIUM_TIMED_EVENT"})
+
+    -- Minigame Config
     local cfg = {
         focus = true, -- Should minigame take nui focus
         cursor = true, -- Should minigame have cursor  (required for lockpick)
@@ -119,33 +141,79 @@ AddEventHandler('bcc-robbery:LootHandler', function(e)
     end
 
     while true do
-        Wait(5)
-        if PlayerDead then break end
+        Wait(0)
 
+        local playerPed = PlayerPedId()
+        local playerCoords = GetEntityCoords(playerPed)
+        local distance = #(playerCoords - lootCfg.LootCoordinates)
 
-        local plc = GetEntityCoords(PlayerPedId())
-        
-        local dist = GetDistanceBetweenCoords(plc.x, plc.y, plc.z, e.LootCoordinates.x, e.LootCoordinates.y, e.LootCoordinates.z, true)
-        if dist < 6 then
-            BccUtils.Misc.DrawText3D(e.LootCoordinates.x, e.LootCoordinates.y, e.LootCoordinates.z, _U('Robbery'))
+        if (distance > 30) or (IsEntityDead(playerPed)) then
+            DoReset = true
+            break
         end
-        if dist < 2 then
-            PromptGroup:ShowGroup(_U('Robbery'))
-            if firstprompt:HasCompleted() then
+
+        if distance < 6 then
+            BccUtils.Misc.DrawText3D(lootCfg.LootCoordinates.x, lootCfg.LootCoordinates.y, lootCfg.LootCoordinates.z, _U('Robbery'))
+        end
+
+        if distance < 2 then
+            lootGroup:ShowGroup(_U('Robbery'))
+            if lootPrompt:HasCompleted() then
 
                 MiniGame.Start('lockpick', cfg, function(result)
                     if result.unlocked then
-                        if e.CashReward > 0 then
-                            TriggerServerEvent('bcc-robbery:CashPayout', e.CashReward)
+                        if lootCfg.CashReward > 0 then
+                            TriggerServerEvent('bcc-robbery:CashPayout', lootCfg.CashReward)
                         end
-                        TriggerServerEvent('bcc-robbery:ItemsPayout', e)
+                        TriggerServerEvent('bcc-robbery:ItemsPayout', lootCfg)
                         Inmission = false
                     else
-                        VORPcore.NotifyRightTip(_U('PickFailed'), 4000)
+                        Core.NotifyRightTip(_U('PickFailed'), 4000)
                         Inmission = false
                     end
-                end) break
+                end)
+                break
             end
+        end
+    end
+    if DoReset then
+        ResetRobbery()
+        return
+    end
+end)
+
+local function LoadModel(hash, model)
+    if not IsModelValid(hash) then
+        return print('Invalid model:', model)
+    end
+
+    RequestModel(hash, false)
+    while not HasModelLoaded(hash) do
+        Wait(10)
+    end
+end
+
+AddEventHandler('bcc-robbery:EnemyPeds', function(location)
+    local NpcCoords = location.EnemyNpcCoords
+    local model = location.NpcModel
+    local hash = joaat(model)
+    local enemyPeds = {}
+
+    LoadModel(hash, model)
+
+    for k, coords in pairs(NpcCoords) do
+        enemyPeds[k] = Citizen.InvokeNative(0xD49F9B0955C367DE, hash, coords.x, coords.y, coords.z, 0.0, true, false, false, false) -- CreatePed
+        Citizen.InvokeNative(0x283978A15512B2FE, enemyPeds[k], true) -- SetRandomOutfitVariation
+        Citizen.InvokeNative(0xF166E48407BAC484, enemyPeds[k], PlayerPedId(), 0, 0) -- TaskCombatPed
+        Citizen.InvokeNative(0x23f74c2fda6e7c61, 953018525, enemyPeds[k]) -- BlipAddForEntity
+    end
+    while true do
+        Wait(1000)
+        if DoReset then
+            for _, ped in pairs(enemyPeds) do
+                DeletePed(ped)
+            end
+            break
         end
     end
 end)
