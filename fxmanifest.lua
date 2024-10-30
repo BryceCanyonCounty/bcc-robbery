e1a30537bb23adafbf84289825cec938e5c387f8
Apollyon <37031311+JusCampin@users.noreply.github.com>
2024-10-30 15:20:08 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ fx_version "cerulean"
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 lua54 'yes'
-author 'Jake2k4'
+author 'BCC Team'
 
 shared_scripts {
     'configs/*.lua',
@@ -27,5 +27,4 @@ dependency {
     'bcc-job-alerts'
 }
 
-
-version '1.1.0'
+version '1.1.1'
